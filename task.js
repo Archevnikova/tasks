6085e99ccf1789f4f8682971a237d0a5102a787a
Archevnikova <ms.orlovakatya@mail.ru>
2023-10-27 10:02:44 +0300

    Пробные задачки

---
+++ task.js
@@ -0,0 +1,26 @@
+/*
+company = {
+    post:"developer",
+    name:"Ivan"
+}
+alert(company.name);
+
+let students = ['Алексей', 'Иван', 'Александр'];
+
+alert(students[1]);*/
+
+/*Напишите функцию showMessage, которая будет содержать переменную message со значением "Привет, ". 
+Создайте переменную userName со значением "Вася" или любое другое имя 
+вначале кода за пределами функции и добавьте эту переменную к значению переменной message. Выведите результат в alert.*/
+/*
+let userName = "Вася"; 
+function showMessage(){
+    let message = "привет,"+ userName;
+    alert( message);
+}
+showMessage();
+
+Напишите код, который создает кнопку, повешайте на нее событие onclick, которое при нажатии на кнопку выполняет функцию myFunction.*/
+myFunction(){
+    console.log("ты молодец");
+}
