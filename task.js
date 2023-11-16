1814dc6b02eb7cd186b088021b40af726a574f53
Archevnikova <ms.orlovakatya@mail.ru>
2023-11-16 21:43:44 +0300

    три последних задачи на циклы не решены

--- task.js
+++ task.js
@@ -206,7 +206,19 @@ function palindrome(){
 // 6. Написать конвертор валют. Пользователь вводит количество USD, выбирает, в какую валюту хочет перевести: EUR,
 // UAN или AZN, и получает в ответ соответствующую сумму
 function money(){
-
+    let inputt = document.querySelector (".in");
+    let select = document.querySelector ("select");
+    let out = document.querySelector (".out");
+    let eur = 0.93;
+    let uan = 36.04;
+    let resalt = 0;
+    if ( select.value === "eur"){
+        resalt = inputt.value * eur;  
+    }else {
+         resalt =inputt.value * uan; 
+    }
+    
+    out.textContent = resalt;
 }
 
 
@@ -234,7 +246,7 @@ let c = prompt ("длина окружности");
 let p = prompt ("периметр квадрата");
 c = Number (c);
 p = Number (p);
-if (c <= p){
+if (c <= p/4){
     console.log("поместится");
 }else {
     console.log("не поместится");
@@ -258,39 +270,269 @@ console.log(ball);
 // месяц, год, а также високосный год. 
 function date (){
     let dateOne = prompt ("первая дата");
-    dateOne = Date(dateOne)
+    dateOne = Date(dateOne);
+    console.log (dateOne);
+    let nextDay = dateOne.getDate()+1;
+   // let nextDay = nextDay.setDate(day.getDate() + 1);
+    console.log(nextDay)
 }
 
 
 
 //модуль 1.3
 
+//1 Подсчитать сумму всех чисел в заданном пользователем
+//диапазоне. 
+function diapozon (){
+let n = prompt("от");
+let m = prompt("до");
+let sum= 0;
+n= Number (n);
+m= Number (m);
+for (let counter = n; counter <=m;counter++){
+    sum = sum+counter;
+    console.log(counter);
+}
+console.log("сумма",sum);
+}
+//2
+//Запросить 2 числа и найти только наибольший общий
+// делитель. 
+function nod (){
+    let one  = prompt("первое");
+    let two = prompt("второе");
+    one = Number(one);
+    two = Number(two);
+    while (one != 0 && two != 0 ){
+        if (one > two){
+            one = one % two ;
+        }else {
+            two = two % one
+        }
+    }
+    console.log(one + two);               //выводит два числа?
+}
 
 
+//3.Запросить у пользователя число и вывести все делители 
+//этого числа
+function deviderF(){
+    let i = prompt("ввести число");
+    i = Number(i);
+    let deviders =[];
+    for (let devider= 1; devider <=i ; devider ++){
+        if (i % devider ==0){
+            deviders.push(devider);
+            }
+        
+        }
+    console.log(deviders);
+}
+
+//4.Определить количество цифр в введенном числе
+function howManyNumbers (){
+    let i = prompt("ввести число");
+    i = Number(i);
+    let count= 0;
+    while(i !=0 ){
+        i = Math.floor(i/10);    
+
+       count++;
+       console.log(i);
+    }
+    console.log(count);
+}
 
 
-
-
-
-
-
-// Запросить у пользователя 10 чисел и подсчитать, сколько
+//5. Запросить у пользователя 10 чисел и подсчитать, сколько
 // он ввел положительных, отрицательных и нулей. При этом
 // также посчитать, сколько четных и нечетных. Вывести
 // статистику на экран. Учтите, что достаточно одной переменной (не 10) для ввода чисел пользователем
 function wTF (){
 let positiveNumber = 0;
-let evenNumber = 0;
-let noEvenNumber = 0;
 let minusNumber = 0;
 let nol = 0;
+let evenNumber = 0;
+let noEvenNumber = 0;
 for (let n = 0; n <10 ; n++){
     let i = prompt ("введите число");
     i = Number(i);
     if (i > 0 ){
         positiveNumber++;
-    } else if ( i % 2 == 0 ){
+    } else if ( i < 0 ){
         minusNumber++;
+    } else if (i ==0){
+        nol++;
+    }
+     if (i % 2 == 0 ){
+        evenNumber++;
+    }else if (i %2 !=0){
+        noEvenNumber++
+    }
+    }
+    console.log(positiveNumber,minusNumber,nol,evenNumber,noEvenNumber);
+}
+// .6. Зациклить калькулятор. Запросить у пользователя 2 числа
+// и знак, решить пример, вывести результат и спросить, хочет ли он решить еще один пример. И так до тех пор, пока
+// пользователь не откажется.
+
+function calculator(){
+    while ( true ){
+        let one  = prompt("первое");
+        let two = prompt("второе");
+        let znak = prompt("знак");
+        one = Number(one);
+        two = Number(two);
+        if (znak == "+" ){
+            console.log (one + two);
+        } else if (znak == "-"){
+            console.log (one - two);
+        } else if (znak == "/"){   
+            console.log(one / two);
+        }else if (znak == "*" ){  
+            console.log(one * two);
+        }
+        let i = prompt("решить еще один?");
+        if (i == "no") {
+            break;
+            
+        }
     }
 }
+
+function cal(){
+    while ( true ){
+        let one  = prompt("первое");
+        let two = prompt("второе");
+        let znak = prompt("знак");
+        one = Number(one);
+        two = Number(two);
+        switch (znak){
+            case "+":
+                console.log(one + two);
+                break;
+            case "-":
+                 console.log (one - two) ; 
+                 break;  
+            case "/":
+                 console.log (one / two);
+                 break;
+            case "*":
+                console.log (one * two);
+                break;
+        }
+        let i = prompt("решить еще один?");
+        if (i == "no") {
+            break;
+            
+        }
+    }
 }
+
+//7. Запросить у пользователя число и на сколько цифр его
+// сдвинуть. Сдвинуть цифры числа и вывести результат (если
+//     число 123456 сдвинуть на 2 цифры, то получится 345612)
+// (количество цифр в числе минус - смещение )* 10)
+function unwrap(){
+let n = prompt("число");
+n = Number(n);
+let s = Number(prompt("смещение"));
+let i = Math.floor(n/100);//тервые три
+let lost = n % 10;//единицы
+let penultimate = Math.floor((n % 100 ) /10 ) *10;//десятки
+console.log("lost" + "penultimate"+i);
+}
+
+function unwrapTwo(){
+let a = n;
+let count= 0;
+while(n !=0 ){            
+    n = Math.floor(n/10);    //заберает последнюю цифру пока n не станет= 0
+   count++;                  //и считает сколько раз он это сделал.Столько цифр в числе
+   
+}
+ let resalt =(count - i)*10;
+ console.log (Math.floor(a / resalt));
+}
+
+
+// 8. Зациклить вывод дней недели таким образом: «День недели.
+// Хотите увидеть следующий день?» и так до тех пор, пока
+// пользователь нажимает OK. 
+function week(){
+    while(true){
+        let day = prompt("День недели" ,"Хотите увидеть следующий день?");
+        let week = ["mon","tues","wedn","thurs","fri","satur","sun"];
+        while(day= true){
+        week++                        //for in?
+            console.log(`${week}`);
+        }  
+    }    
+ } 
+
+//9. Вывести таблицу умножения для всех чисел от 2 до 9.
+// Каждое число необходимо умножить на числа от 1 до 10.
+function table (){
+    result = 0;
+    for (let one = 1; one <=10; one ++){
+        for (let count = 1; count <= 10; count ++ ){
+            result = one * count;
+            console.log( `${one} * ${count} = ${result}` );
+        }
+    }
+}
+// 10. Игра «Угадай число». Предложить пользователю загадать
+// число от 0 до 100 и отгадать его следующим способом:
+// каждую итерацию цикла делите диапазон чисел пополам,
+// записываете результат в N и спрашиваете у пользователя
+// «Ваше число > N, < N или == N?». В зависимости от того
+// что указал пользователь, уменьшаете диапазон. Начальный
+// диапазон от 0 до 100, поделили пополам и получили 50.
+// Если пользователь указал, что его число > 50, то изменили
+// диапазон на от 51 до 100. И так до тех пор, пока пользователь не выберет == N.
+  let n = Number( prompt("загадай число от 0 до 100"));
+  let centerRange = (n >0 || n < 100 )/2;
+    for (let a = 0; a <=100; a++){
+        let result = (a + 100)/2;
+        }
+        console.log (result);
+    // 63>50 
+    // от 50 до 100
+    // (500+100)/2 = 75 цикл
+    // 63 < 75
+    // от 50 до 75
+    // (50+75)/2 = 62
+    // 62 < 63
+    // от 62 до 75
+    // (62 + 75)/2 = 68
+    // 63< 68
+    // от 62 до 68
+    // (62 + 68)/2= 65
+    // 63 < 65
+    // от 62 до 65
+    // (62 + 65)/2 = 63
+    // 63 ==63
+    // console.log();
+
+    
+
+
+
+    
+
+    //     for (let range2 = 51; range2 <=100; range2 ++){ 
+    //         let a = range2 / 2 ;
+    //         };
+    // } else ( n < range ){
+        
+    // }
+
+    //     }
+
+    //     console.log("Красавчик!");
+    // }
+    
+
+
+
+
